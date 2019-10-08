6f80a906d4225c69a5d84fca02c1b6dfa6b29cd7
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2019-10-08 11:36:11 +0200

    Rewritten download_ancestors.py to accept args

--- download_ancestors.py
+++ download_ancestors.py
@@ -1,13 +1,84 @@
 from lxml import html
 from urllib.parse import urljoin
 from urllib.request import urlopen
-import os, requests, sys, urllib
+import argparse, errno, os, requests, sys, urllib
 
-url = 'http://www.antenati.san.beniculturali.it/v/Archivio+di+Stato+di+Forli/Stato+civile+italiano/Forliprovincia+di+Forli-Cesena/Matrimoni/1876/10/100546507_00301.jpg.html?g2_imageViewsIndex=0'
-inverse_order = False
-full_resolution = False
-test_run = False
+def extract_info(args):
+    # URL example:
+    # http://www.antenati.san.beniculturali.it/v/Archivio+di+Stato+di+Forli/Stato+civile+italiano/Forliprovincia+di+Forli-Cesena/Matrimoni/1876/10/100546507_00301.jpg.html?g2_imageViewsIndex=0
+    url = args.URL
+    protocol = 'http://'
 
+    # Anything after '?' is useless to us
+    if '?' in url:
+        url = url[:url.rfind('?')]
+
+    # Splitting our URL by '/'
+    split_url = url.split('/')
+
+    # How every filename will start and end
+    filename_head = split_url[-1][:split_url[-1].find('_') + 1]
+    filename_tail = split_url[-1][split_url[-1].find('.'):]
+
+    # Get the last page number, get how many zeros and trim them
+    last_file = split_url[-1][split_url[-1].find('_') + 1 : split_url[-1].find('.')]
+    padding = len(last_file)
+    last = int(last_file.lstrip('0'))
+
+    # Base URL of every page
+    base_url = protocol + '/'.join(split_url[2:10])
+
+    # Then a list of self-explanatory variables
+    registry_nr = split_url[9]
+    year = split_url[8]
+    registry_type = split_url[7]
+    city = split_url[6]
+    archive_type = split_url[5]
+    archive_city = split_url[4]
+    root = protocol + '/'.join(split_url[2:3])
+
+    return {
+        'url': url,
+        'filename_head': filename_head,
+        'filename_tail': filename_tail,
+        'last': last,
+        'padding': padding,
+        'base_url': base_url,
+        'registry_nr': registry_nr,
+        'year': year,
+        'registry_type': registry_type,
+        'city': city,
+        'archive_type': archive_type,
+        'archive_city': archive_city,
+        'root': root
+    }
+
+def setup_args():
+    parser = argparse.ArgumentParser(description='Download archives')
+    parser.add_argument('-t', '--test-run', action='store_true')
+    parser.add_argument('-f', '--full-resolution', action='store_true')
+    parser.add_argument('-i', '--inverse-order', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('URL')
+    return parser.parse_args()
+
+def setup_download(args):
+    url_info = extract_info(args)
+    parameters = dict()
+    parameters['headers'] = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11' }
+    if args.full_resolution is True:
+        parameters['prefix'] = '/'
+    else:
+        parameters['prefix'] = '/thumb_'
+    if args.inverse_order is True:
+        parametersons['step'] = -1
+        parameters['first'] = url_info['last']
+        parameters['last'] = 1
+    else:
+        parameters['step'] = 1
+        parameters['first'] = 1
+        parameters['last'] = url_info['last']
+    return {**url_info, **parameters}
 
 def write_link():
     file_name = 'link.URL'
@@ -15,74 +86,50 @@ def write_link():
 ##    link.write('[InternetShortcut]\nURL=' + full_url)
 ##    link.close
 
-def extract_info():
-    global url, tail, base, padding, last, registry_nr, year, registry, city, archive_type, archive, root, full_url
-    url = url[:url.rfind('?')]
-    url_filename = url[url.rfind('/'):]
-    tail = url_filename[url_filename.find('.'):]
-    base = url_filename[:url_filename.find('_') + 1]
-    last_file = url_filename[url_filename.find('_') + 1 :url_filename.find('.')]
-    padding = len(last_file)
-    last = int(last_file.lstrip('0'))
-    url = url[:url.rfind('/')]
-    full_url = url
-    registry_nr = url[url.rfind('/'):]
-    url = url[:url.rfind('/')]
-    year = url[url.rfind('/'):]
-    url = url[:url.rfind('/')]
-    registry = url[url.rfind('/'):]
-    url = url[:url.rfind('/')]
-    city = url[url.rfind('/'):-25]
-    url = url[:url.rfind('/')]
-    archive_type = url[url.rfind('/'):]
-    url = url[:url.rfind('/')]
-    archive = url[url.rfind('/'):]
-    url = url[:url.rfind('/')]
-    root = url[:url.rfind('/')]
+def main():
+    args = setup_args()
+    download_parameters = setup_download(args)
+    print('Downloading %s from %s(%s)' % (download_parameters['registry_type'], download_parameters['year'], download_parameters['registry_nr']))
 
-def setup():
-    global headers, prefix, step, first
-    extract_info()
-    headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11' }
-    prefix = '/'
-    step = 1
-    first = 1
-    if thumbnails is True:
-        prefix = '/thumb_'
-    if inverse_order is True:
-        first, last = last, first
-        step = -1
-    print('Downloading %s from %s(%s)' % (registry[1:], year[1:], registry_nr[1:]))
+    if args.full_resolution is False:
+        write_link() # TODO
 
-setup()
-if thumbnails is True:
-    write_link()
-
-for i in range(first, last + 1, step):
-    current_url = full_url + base + str(i).zfill(padding) + tail
-    current_file = '.' + city + registry + year + registry_nr + prefix + str(i).zfill(len(str(last))) + '.jpg'
-    try:
-        page = requests.get(current_url, headers=headers)
-    except requests.exceptions.RequestException as e:
-        print(e)
-        sys.exit(1)
-    tree = html.fromstring(page.content)
-    if full_resolution is True:
-        img = tree.xpath('//a[contains(@class, "cloud-zoom")]/@href')
-        img_url = img[-1]
-    else:
-        img = tree.xpath('//a[contains(@class, "cloud-zoom")]/img/@src')
-        img_url = urljoin(root, img[-1])
-    print("Downloading %s/%s" % (str(i).zfill(len(str(last))), last), end='\n')
-    img_file = urlopen(img_url)
-    if not os.path.exists(os.path.dirname(current_file)):
+    for i in range(download_parameters['first'], download_parameters['last'] + 1, download_parameters['step']):
+        current_url = '/'.join([download_parameters['base_url'], download_parameters['filename_head'] + str(i).zfill(download_parameters['padding']) + download_parameters['filename_tail']])
+        current_file = './' + '/'.join([download_parameters['city'], download_parameters['registry_type'], download_parameters['year'], download_parameters['registry_type'], download_parameters['prefix'] + str(i).zfill(len(str(download_parameters['last']))) + '.jpg'])
         try:
-            os.makedirs(os.path.dirname(current_file))
-        except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
-    output = open(current_file, "wb")
-    output.write(img_file.read())
-    output.close()
+            if args.verbose is True:
+                print('Opening URL:', current_url)
+            page = requests.get(current_url, headers=download_parameters['headers'])
+        except requests.exceptions.RequestException as e:
+            print(e)
+            sys.exit(1)
+        tree = html.fromstring(page.content)
+        if args.full_resolution is True:
+            img = tree.xpath('//a[contains(@class, "cloud-zoom")]/@href')
+            img_url = img[-1]
+        else:
+            img = tree.xpath('//a[contains(@class, "cloud-zoom")]/img/@src')
+            img_url = urljoin(download_parameters['root'], img[-1])
+        print("Downloading %s/%s" % (str(i).zfill(len(str(download_parameters['last']))), download_parameters['last']), end='\n')
+        
+        if args.verbose is True:
+            print('Opening image URL:', img_url)
+        img_file = urlopen(img_url)
+        if args.test_run is False:
+            if args.verbose is True:
+                print('Writing file', img_url)
+            if not os.path.exists(os.path.dirname(current_file)):
+                try:
+                    os.makedirs(os.path.dirname(current_file))
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+            output = open(current_file, "wb")
+            output.write(img_file.read())
+            output.close()
 
-print("Done.")
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
